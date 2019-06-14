e73ed0108db4af2b54acbebc4ad01e7c835ab727
Somil Singhai <somilsinghai1804@gmail.com>
2019-06-15 01:03:41 +0530

    Added the first scipt
    The script helps to make a VM instance and make its IP static.

---
+++ Create_VM.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+name=$1
+
+if [[ -n "$name" ]]; then
+    echo "Creating VM"
+else
+    echo "argument error"
+fi
+
+PROJECT="yesbankdatathon"
+MACHINE="n1-standard-1"
+REGION="europe-west1"
+ZONE="europe-west1-c"
+ACCOUNT="143321348218-compute@developer.gserviceaccount.com"
+IMAGE="ubuntu-1804-bionic-v20190612"
+DISKTYPE="pd-ssd"
+DISKSIZE="200GB"
+
+APIS="https://www.googleapis.com/auth/pubsub,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/trace.append,https://www.googleapis.com/auth/devstorage.full_control"
+
+
+gcloud compute --project=$PROJECT instances create $name --zone=$ZONE --machine-type=$MACHINE --subnet=default \
+--network-tier=PREMIUM --maintenance-policy=MIGRATE --service-account=$ACCOUNT --scopes=$APIS \
+--image=$IMAGE --image-project=ubuntu-os-cloud --boot-disk-size=$DISKSIZE \
+--boot-disk-type=$DISKTYPE --boot-disk-device-name=$name
+
+
+echo "Making the external IP static"
+
+EXTIPADD=$(gcloud compute instances describe $name --format='get(networkInterfaces[0].accessConfigs[0].natIP)' --zone=$ZONE)
+
+gcloud compute addresses create $name --addresses $EXTIPADD --region=$REGION
+
+echo $EXTIPADD
